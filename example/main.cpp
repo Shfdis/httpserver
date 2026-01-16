699a17a58258ec562af387b1ce8387d0491e09ae
Shifdis <huaweitit0101@gmail.com>
2026-01-16 19:32:02 +0300

    abstract out the async io

--- example/main.cpp
+++ example/main.cpp
@@ -1,30 +1,30 @@
 #include "http_server.h"
+#include "iasio.h"
+#include "io_uring.h"
 #include <future>
 #include <string_view>
 
 int main() {
   HTTP::ServerBuilder builder;
   builder.SetPort(8080);
+  builder.AddRequest(HTTP::POST, "/echo", [](const HTTP::RequestData &request) {
+    HTTP::ResponseData response;
+    response.status = 200;
+    response.body = request.body;
+    return response;
+  });
 
-  builder.AddRequest(HTTP::POST, "/echo",
-                     [](const HTTP::RequestData &request) {
-                       HTTP::ResponseData response;
-                       response.status = 200;
-                       response.body = request.body;
-                       return response;
-                     });
-
-  builder.AddRequest(HTTP::GET, "/echo",
-                     [](const HTTP::RequestData &request) {
-                       HTTP::ResponseData response;
-                       response.status = 200;
-                       auto it = request.params.find("msg");
-                       if (it != request.params.end()) {
-                         response.body = it->second;
-                       }
-                       return response;
-                     });
-
+  builder.AddRequest(HTTP::GET, "/echo", [](const HTTP::RequestData &request) {
+    HTTP::ResponseData response;
+    response.status = 200;
+    auto it = request.params.find("msg");
+    if (it != request.params.end()) {
+      response.body = it->second;
+    }
+    return response;
+  });
+  builder.SetAsio(
+      std::unique_ptr<HTTP::IAsio>((HTTP::IAsio *)new HTTP::IOUring()));
   auto server = builder.Build();
   server.Start();
   std::promise<void> keep_alive;
